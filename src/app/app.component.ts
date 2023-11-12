7c61c7674fb46c149f1b213fb6dc8f80bdc686c5
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-11-12 10:37:30 +0100

    Mise en place routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'car-sell';
+
+  cars = [
+    {
+      id: 0,
+      brand: 'Renault',
+      model: 'Laguna',
+      color: 'gray'
+    },
+    {
+      id: 1,
+      brand: 'Peugeot',
+      model: '508',
+      color: 'red'
+    },
+    {
+      id: 2,
+      brand: 'Opel',
+      model: 'Corsa',
+      color: 'blue'
+    }
+  ];
+
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
