b733c510273e6b6c954801ada1b537e5e02fc164
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2023-11-16 09:34:45 +0100

    add registerLocaleData

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -7,6 +7,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr'
+
+registerLocaleData(localeFr)
 
 @NgModule({
   declarations: [
@@ -21,7 +25,9 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
